d4c014524a904a456211d5fdd84c0bd9298fcd8e
Camilo Lehue <clehue@ast.cl>
2025-04-22 17:34:32 -0400

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,19 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: ["react-leaflet"],
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://dashgps.iotlink.cl",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    host: "0.0.0.0", 
+    port: 5186,
+    allowedHosts: ["dashgps.iotlink.cl"], 
+  },
 });
