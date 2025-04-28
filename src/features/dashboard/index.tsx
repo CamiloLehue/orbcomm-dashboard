88ebc65fd159c79754adfb686625b5e9b0693b11
Camilo Lehue <clehue@ast.cl>
2025-04-28 17:33:39 -0400

    interfaz del dashboard principal

--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -1,16 +1,140 @@
-import TopBar from "./TopBar";
-import LeftBar from "./LeftBar";
-import Content from "./Content";
+import { GrChat, GrDocumentPdf, GrFormNextLink, GrPhone, GrSend } from "react-icons/gr";
+import Button from "../../components/ui/Button";
+import MapSvg from "../../components/mapsold/MapSvg";
 
 function Dashboard() {
   return (
     <div className="relative h-full w-full flex flex-col justify-start items-start">
-      <TopBar />
-      <div className="relative min-h-170 h-full w-full flex justify-start items-start gap-5">
-        <LeftBar />
-        <Content />
+      <h1 className="text-white pb-5">Escritorio Principal</h1>
+      <div className="grid grid-cols-12 gap-5 w-full h-full">
+        <article className="relative bg-bgs col-span-3 flex flex-col gap-2 border border-bgt rounded-2xl">
+          <div className="absolute w-60 h-60 bg-secondary/40  blur-3xl top-0 left-0 rounded-full"></div>
+          <div className="relative bg-bgp/60 text-white  border-t border-bgs w-full h-full  rounded-2xl flex flex-col justify-start items-start gap-2">
+            <h3 className="absolute top-3 left-5 font-light">Entrega</h3>
+            <h5 className="absolute top-8 left-5 font-light text-xs text-gray">
+              en curso
+            </h5>
+            <div className="relative flex justify-between items-center mt-10 px-10 py-5  w-full border-b border-bgt/90">
+              <div>
+                <small className="text-primary">Número de envio</small>
+                <h2 className="font-bold tracking-wider">E-20050006</h2>
+              </div>
+              <div className="relative">
+                <div className="absolute top-0 left-0 w-full h-full blur-3xl bg-zinc-200/50 rounded-full"></div>
+                <img src="dashboard/truck.png" alt="mapa" className="relative w-50" />
+              </div>
+            </div>
+            <div className="relative w-full px-15 my-5">
+              <div className=" border-b-2 border-gray ">
+                <div className="absolute bg-secondary -top-2.5 h-5 w-5 rounded-full">
+                  <small className="absolute top-5 -left-2 text-gray">Origen</small>
+                  <h4 className="absolute top-9 -left-3 text-nowrap">Castro</h4>
+                </div>
+                <div className="absolute bg-primary -top-3 right-15 h-6 w-6 rounded-full">
+                  <small className="absolute top-5 -left-2 text-gray">Origen</small>
+                  <h4 className="absolute top-9 -left-9 text-nowrap">Puerto Montt</h4>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div className="flex items-center justify-center gap-2 pb-1 text-xs">
+            <Button rounded="lg" className=" hover:bg-bgt px-5 gap-2  ">
+              <GrPhone size={15} />
+              Llamar cliente
+            </Button>
+            <Button rounded="lg" className=" hover:bg-bgt px-5 gap-2  ">
+              <GrSend size={15} />
+              Correo
+            </Button>
+            <Button rounded="lg" className=" hover:bg-bgt px-5 gap-2  ">
+              <GrChat size={15} />
+              Chat
+            </Button>
+          </div>
+        </article>
+        <article className="relative bg-bgs col-span-3 flex flex-col gap-2 border border-bgt rounded-2xl">
+          <div className="relative bg-bgp/60 text-white  border-t border-bgs w-full h-full  rounded-2xl flex flex-col justify-center items-center gap-2">
+            <h3 className="absolute top-3 left-5 font-light">Transportes</h3>
+            <h5 className="absolute top-8 left-5 font-light text-xs text-gray">en línea</h5>
+            <div className=" flex flex-col justify-center items-center gap-2   px-10 py-5 rounded-2xl">
+              <h4 className="text-9xl">8</h4>
+              <small>Rastreando viajes</small>
+            </div>
+            <div className="grid grid-cols-3 gap-2  py-5 border border-bgs px-10 rounded-2xl">
+              <div className="flex flex-col w-full justify-center items-center">
+                <p className="text-danger">1</p>
+                <small>Sin conexión</small>
+              </div>
+              <div className="flex flex-col w-full  justify-center items-center">
+                <p className="text-warning">8</p>
+                <small>Pendientes</small>
+              </div>
+              <div className="flex flex-col w-full  justify-center items-center">
+                <p className="text-primary">12</p>
+                <small>Finalizados</small>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center justify-center gap-2 pb-1 text-xs">
+            <Button rounded="lg" className="bg-bgt hover:bg-bgp px-5  ">
+              Crear nuevo viaje
+            </Button>
+            <Button rounded="lg" className=" hover:bg-bgt px-5  ">
+              Lista +10
+            </Button>
+          </div>
+        </article>
+        <article className="col-span-6 rounded-2xl bg-bgs border-t border-bgt flex flex-col gap-2 w-full">
+          <div className="relative   w-full h-full max-h-[340px]  flex flex-col justify-center items-center gap-2">
+            <h3 className="absolute top-3 left-5 font-light">Mapa</h3>
+            <h5 className="absolute top-8 left-5 font-light text-xs text-gray">rastreo en línea</h5>
+            <MapSvg />
+          </div>
+          <div className="flex items-center gap-2 text-xs">
+
+          </div>
+        </article>
+        <article className="col-span-3 flex flex-col gap-2">
+          <div className="relative bg-bgs border-t border-bgt w-full h-full  rounded-2xl flex flex-col justify-center items-center gap-2 pt-10">
+            <h3 className="absolute top-3 left-5 font-light">Reportes</h3>
+            <h5 className="absolute top-8 left-5 font-light text-xs text-gray">rápidos</h5>            <ul className="flex flex-col gap-2 w-full p-2">
+              {
+                Array(5).fill(0).map((_, i) => (
+                  <li key={i} className="relative bg-bgp rounded-lg px-2 py-2 flex justify-start items-center gap-2">
+                    <span className="px-4 py-1 bg-bgs border-t border-bgt  rounded-sm">
+                      <GrDocumentPdf />
+                    </span>
+                    <p className="font-light">
+                      Documento e{i + 1}.
+                    </p>
+                    <button className="absolute right-5 px-2 py-2 rounded-xl border-t border-bgt bg-primary text-bgp ">
+                      <GrFormNextLink />
+                    </button>
+                  </li>
+                ))
+              }
+
+            </ul>
+          </div>
+          <div className="flex items-center gap-2 text-xs">
+            <Button rounded="lg" className=" hover:bg-bgt px-5  ">
+              Ver más
+            </Button>
+            <Button rounded="lg" className=" hover:bg-bgt px-5  ">
+              Contactar con soporte
+            </Button>
+          </div>
+        </article>
+        <article className="col-span-4 rounded-2xl bg-bgs border-t border-bgt flex flex-col gap-2 w-full">
+          <div className="relative w-full h-full max-h-[380px]  flex flex-col justify-center items-center gap-2">
+            <h3 className="absolute top-3 left-5 font-light">Línea de transporte</h3>
+            <h5 className="absolute top-8 left-5 font-light text-xs text-gray">rastreo en línea</h5>
+            <img src="dashboard/chartLine.png" alt="mapa" className="w-full h-full" />
+          </div>
+        </article>
       </div>
-    </div>
+    </div >
   );
 }
 
