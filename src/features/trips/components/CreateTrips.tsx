b35a4e78abb61e5a741cf27fdbf7256100d0c5a5
Camilo Lehue <clehue@ast.cl>
2025-04-30 16:03:45 -0400

    interfaz v2

--- src/features/trips/components/CreateTrips.tsx
+++ src/features/trips/components/CreateTrips.tsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+import Content from "./Content";
+import TopBar from "./TopBar";
 
 function CreateTrips() {
-  return (
-    <div>CreateTrips</div>
-  )
+    const [origin, setOrigin] = useState<[number, number] | null>(null);
+    const [destination, setDestination] = useState<[number, number] | null>(null);
+
+    const handleRouteRequested = (newOrigin: [number, number], newDestination: [number, number]) => {
+        setOrigin(newOrigin);
+        setDestination(newDestination);
+    };
+
+    return (
+        <div className="relative h-full w-full flex flex-col gap-5 justify-start items-start  ">
+            <div className="absolute top-0 right-0 bg-secondary/10 h-100 w-100 blur-3xl rounded-full"></div>
+            <h1 className="text-white">Nuevo Viaje</h1>
+            <div className="flex justify-center items-center gap-5 w-full">
+                <div className="">
+                    <TopBar onRouteRequested={handleRouteRequested} />
+                </div>
+                <Content origin={origin} destination={destination} />
+            </div>
+        </div>
+    );
 }
 
-export default CreateTrips
+export default CreateTrips;
--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,43 +1,143 @@
-import { GrDeliver, GrGateway, GrNavigate, GrProjects } from "react-icons/gr"
+import { GrDeliver, GrGateway, GrNavigate, GrProjects, GrFormNext } from "react-icons/gr"
 import Button from "../components/ui/Button"
 import { useNavigate } from "react-router"
+import { useState } from "react";
 
 function Sidebar() {
-
     const navigate = useNavigate();
+    // const location = window.location.pathname;
+    const menu = [
+        {
+            icon: GrProjects,
+            text: "Inicio",
+            link: "/",
+        },
+        {
+            icon: GrDeliver,
+            text: "Viajes",
+            link: "/viajes",
+            submenu: [
+                {
+                    icon: GrProjects,
+                    text: "Inicio",
+                    link: "/"
+                },
+                {
+                    icon: GrProjects,
+                    text: "Inicio",
+                    link: "/"
+                },
+            ]
+        },
+        {
+            icon: GrGateway,
+            text: "Monitoreo",
+            link: "/monitoreo",
+            submenu: [
+                {
+                    icon: GrProjects,
+                    text: "Inicio",
+                    link: "/"
+                },
+                {
+                    icon: GrProjects,
+                    text: "Inicio",
+                    link: "/"
+                },
+            ]
+        },
+        {
+            icon: GrNavigate,
+            text: "Tramos",
+            link: "/tramos",
+            submenu: [
+                {
+                    icon: GrProjects,
+                    text: "Inicio",
+                    link: "/"
+                },
+                {
+                    icon: GrProjects,
+                    text: "Inicio",
+                    link: "/"
+                },
+            ]
+        },
+    ]
+    const [openSidebar, setOpenSidebar] = useState(false);
+
+
     return (
-        <div className=" w-[100px] bg-gradient-to-b from-secondary/10 border-t border-secondary/20 rounded-2xl py-5 flex flex-col justify-start items-center gap-5">
+        <div className={`${!openSidebar ? `w-[100px]` : `w-[200px]`} relative transition-all duration-200 bg-gradient-to-b from-secondary/10 border-t border-secondary/20 rounded-2xl py-5 flex flex-col justify-start items-center gap-5`}>
+            <div className="absolute -right-15 top-8.5">
+                <Button onClick={() => setOpenSidebar(!openSidebar)} className="relative z-50 text-xs text-zinc-500 flex  items-center justify-start  gap-2">
+                    {
+                        !openSidebar ? <GrFormNext size={20} className="text-secondary" /> : <GrFormNext size={20} className="text-primary rotate-180" />
+                    }
+                </Button>
+
+            </div>
             <h6 className="text-zinc-600 text-center font-bold">
                 Menu
             </h6>
             <nav>
-                <ul className="flex flex-col gap-5 w-full justify-center items-center ">
+                <ul className="flex flex-col gap-5 w-full justify-center items-start">
+                    {
+                        menu.map((item, i) => (
+                            <li key={i} className="relative w-full">
+                                <Button onClick={() => navigate(item.link)} className="text-xs text-zinc-500 flex  items-center justify-start  gap-2">
+                                    <item.icon size={20} />
+                                    {
+                                        openSidebar && item.text
+                                    }
+                                </Button>
+                                {/* {
+                                    item.submenu && item.link === location && (
+                                        <div key={i} className="absolute top-0 hover:bg-primary -right-60 z-10 bg-gray w-50 rounded-sm p-5">
+                                            1
+                                        </div>
+                                    )
+                                } */}
+                            </li>
+                        ))
+                    }
+
+                </ul>
+                {/* <ul className="flex flex-col gap-5 w-full justify-center items-start">
                     <li>
-                        <Button onClick={() => navigate("/")} className="text-xs text-zinc-500 flex flex-col items-center justify-center  gap-2">
+                        <Button onClick={() => navigate("/")} className="text-xs text-white flex  items-center justify-start  gap-2">
                             <GrProjects size={20} />
-                            Inicio
+                            {
+                                openSidebar && "Inicio"
+                            }
                         </Button>
                     </li>
                     <li>
-                        <Button onClick={() => navigate("/viajes")} className="text-xs text-zinc-500 flex flex-col items-center justify-center  gap-2">
+                        <Button onClick={() => navigate("/viajes")} className="text-xs text-zinc-500 flex  items-center justify-start  gap-2">
                             <GrDeliver size={20} />
-                            Viajes
+                            {
+                                openSidebar && "Viajes"
+                            }
                         </Button>
                     </li>
                     <li>
-                        <Button onClick={() => navigate("/monitoreo")} className="text-xs text-zinc-500 flex flex-col items-center justify-center  gap-2">
+                        <Button onClick={() => navigate("/monitoreo")} className="text-xs text-zinc-500 flex  items-center justify-start  gap-2">
                             <GrGateway size={20} />
-                            Monitoreo
+                            {
+                                openSidebar && "Monitoreo"
+                            }
                         </Button>
                     </li>
                     <li>
-                        <Button onClick={() => navigate("/viajes")} className="text-xs text-zinc-500 flex flex-col items-center justify-center  gap-2">
+                        <Button onClick={() => navigate("/viajes")} className="text-xs text-zinc-500 flex  items-center justify-start  gap-2">
                             <GrNavigate size={20} />
-                            Tramos
+                            {
+                                openSidebar && "Tramos"
+                            }
                         </Button>
                     </li>
 
-                </ul>
+                </ul> */}
             </nav>
         </div>
     )
